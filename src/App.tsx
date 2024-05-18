9f21e9fec87beea58c7b18c201d6a9ed6aeb050f
Chris Guizzetti <chrisguizzetti@chriss-mbp.lan>
2024-05-18 16:02:09 -0700

    setting up initial contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Contact from "./Contact";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-gradient-to-br from-[#F9FAFB] to-[#D2D6DB] w-screen h-screen flex flex-col justify-center items-center">
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
