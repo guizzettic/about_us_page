0600c72ceeca891fc1d26c8dadeb48c99846badd
Chris Guizzetti <chrisguizzetti@chriss-mbp.lan>
2024-05-19 11:17:30 -0700

    added tailwind prettier plugin for class sorting

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -15,26 +15,26 @@ const Form: React.FC = () => {
   //   });
 
   return (
-    <div className="p-4 bg-white rounded-lg h-[456px] w-[319px] md:h-[398px] md:w-[704px] lg:w-[592px] shadow-lg border-neutral-400 flex flex-col justify-between">
-      <div className="py-1 flex flex-col justify-around h-full mb-5 md:justify-around">
+    <div className="flex h-[456px] w-[319px] flex-col justify-between rounded-lg border-neutral-400 bg-white p-4 shadow-lg md:h-[398px] md:w-[704px] lg:w-[592px]">
+      <div className="mb-5 flex h-full flex-col justify-around py-1 md:justify-around">
         {/* name-input */}
 
         <div className="flex flex-col justify-between md:flex md:flex-row md:justify-between ">
           <div className="flex flex-col md:w-[276px]">
-            <label className="text-sm text-neutral-700 font-medium">Name</label>
+            <label className="text-sm font-medium text-neutral-700">Name</label>
             <input
               type="text"
               name="name"
               onChange={(e) => setForm({ ...form, name: e.target.value })}
               value={form.name}
               placeholder="Your name"
-              className="py-2 px-3 bg-nuetral-500 font-medium border rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="bg-nuetral-500 rounded border px-3 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
           {/* email-input */}
           <div className="flex flex-col md:w-[276px]">
-            <label className="text-sm text-neutral-700 font-medium">
+            <label className="text-sm font-medium text-neutral-700">
               Email
             </label>
             <input
@@ -43,14 +43,14 @@ const Form: React.FC = () => {
               onChange={(e) => setForm({ ...form, email: e.target.value })}
               value={form.email}
               placeholder="example@example.com"
-              className="py-2 px-3 bg-nuetral-500 font-medium border rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="bg-nuetral-500 rounded border px-3 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
         </div>
 
         {/* message-input */}
         <div className="flex flex-col">
-          <label className="text-sm text-neutral-700 font-medium">
+          <label className="text-sm font-medium text-neutral-700">
             Message
           </label>
           <textarea
@@ -60,14 +60,14 @@ const Form: React.FC = () => {
             onChange={(e) => setForm({ ...form, message: e.target.value })}
             value={form.message}
             placeholder="Write your message..."
-            className="py-2 px-3 bg-nuetral-500 font-medium border rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="bg-nuetral-500 rounded border px-3 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
           ></textarea>
           <label className="flex justify-end text-sm font-medium text-neutral-500 underline">
             {form.message.length}/500
           </label>
         </div>
       </div>
-      <button className="bg-indigo-700 text-white w-full rounded-md h-[44px] shadow-md">
+      <button className="h-[44px] w-full rounded-md bg-indigo-700 text-white shadow-md">
         Submit
       </button>
     </div>
